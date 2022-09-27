5ac3c190046694ff5605327729c6c96cc2654e4a
Martial Bornet <martial-externe.bornet@edf.fr>
2022-09-27 22:08:41 +0200

    Added GPL license.

--- src/c/c_msg.h
+++ src/c/c_msg.h
@@ -1,11 +1,28 @@
 /* ============================================================================
- *   (C) Copyright Martial Bornet, 2010.
+ * Copyright (C) 2010-2022, Martial Bornet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  *
  *   Author       :     Martial BORNET (MB) - July 1, 2010
  *
  *   File         :     c_msg.h
  *
- *   @(#)  [Zen] c_msg.h Version 1.48 of 22/09/27 - 
+ *   @(#)  [Zen] c_msg.h Version 1.49 of 22/09/27 - 
+ *
+ * Sources from the original za command are available on :
+ * https://github.com/mbornet-hl/zendiff
  *
  * ============================================================================
  */
