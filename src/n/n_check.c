dc2dc243c9c974859dc94f1c0381eaba75514805
Martial Bornet <martial-externe.bornet@edf.fr>
2022-09-27 22:31:40 +0200

    Added GPL license

--- src/n/n_check.c
+++ src/n/n_check.c
@@ -1,11 +1,28 @@
 /* ============================================================================
- *   (C) Copyright Martial Bornet, 2009-2022
+ * Copyright (C) 2009-2022, Martial Bornet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  *
  *   Author       :     Martial BORNET (MB) - March 29, 2009
  *
  *   File         :     n_check.c
  *
- *   @(#)  [Zen] n_check.c Version 1.4 of 22/09/27 - 
+ *   @(#)  [Zen] n_check.c Version 1.5 of 22/09/27 - 
+ *
+ * Sources from the original za command are available on :
+ * https://github.com/mbornet-hl/zendiff
  *
  * ============================================================================
  */
