f156ce59fb65664a215033114e5eaf509484f66f
mb <mbornet.pro@wanadoo.fr>
2023-09-17 20:12:57 +0200

    Added source code for internationalized messages

---
+++ src/aa/aa_cpub.h
@@ -0,0 +1,32 @@
+/* ============================================================================
+ * Copyright (C) 2009-2023, Martial Bornet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *   Author       :     Martial BORNET (MB) - June 06, 2011
+ *
+ *   File         :     aa_cpub.h
+ *
+ *   @(#)  [Zen] aa_cpub.h Version 1.3 of 23/09/17 - 
+ *
+ * Sources from the original za command are available on :
+ * https://github.com/mbornet-hl/zendiff
+ *
+ * ============================================================================
+ */
+
+#define   AA_MSG(x)                          (N_MAGIC_AA_MSG | (x))
+#define   AA_HTM(x)                          (N_MAGIC_AA_HTM | (x))
+#define   AA_MAN(x)                          (N_MAGIC_AA_MAN | (x))
